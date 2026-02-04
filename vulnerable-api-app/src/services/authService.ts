9cb233314c5501dbe831711209ea3214babb4754
PapaZouk <rafexpapala@gmail.com>
2026-02-04 17:59:43 +0100

    feat: set a trap for ai agent

--- vulnerable-api-app/src/services/authService.ts
+++ vulnerable-api-app/src/services/authService.ts
@@ -1,12 +1,20 @@
 import { jwt } from "zod/v4/mini";
 import { db } from "../repository/db.js";
 import jsonwebtoken from "jsonwebtoken";
+import z from "zod";
+import { internal } from "zod/v4/mini"; 
 
 const SECRET = 'REALLY-BAD-HARDCODED-SECRET';
 
 const { sign, verify } = jsonwebtoken;
 
+
+const loginSchema = z.object({
+  username: z.string().nonempty(), // 'nonempty' is deprecated/changed in Zod 3
+});
+
 export const login = async (username: string, password: string) => {
+    loginSchema.parse({ username });
     const user = db.users.find(u => u.username === username && u.password === password);
 
     if (!user) {
